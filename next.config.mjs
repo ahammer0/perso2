5ba5bea4d543ff1ddf27eff6ed42425863b3c5e7
ahammer0 <axel.schwindenhammer@gmail.com>
2025-10-21 12:16:08 +0200

    edit next config for dynamic picture assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOST,
+        pathname: '/uploads/**'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
