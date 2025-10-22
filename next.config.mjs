3af663b631084d3a046641a4cf0bb4b96e625950
ahammer0 <axel.schwindenhammer@gmail.com>
2025-10-22 18:50:41 +0200

    te

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: new URL(process.env.NEXT_PUBLIC_IMAGE_HOST).hostname,
+        hostname: new URL(process.env.NEXT_PUBLIC_IMAGE_HOST ?? "").hostname,
         pathname: '/api/uploads/**'
       }
     ]
